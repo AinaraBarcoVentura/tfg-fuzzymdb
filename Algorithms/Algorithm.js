0617a01786a7a56753db18d5a9096df54c158c8f
AinaraBarcoVentura <ainaradelbarcoventura@gmail.com>
2018-06-14 21:37:04 +0200

    Version Final

--- Algorithms/Algorithm.js
+++ Algorithms/Algorithm.js
@@ -4,31 +4,28 @@ var Levensthein = require('./Levenshtein.js');
 
 
 function Algorithm(name, n){
-    this.accurate = accurate;
     
-    //Si la propiedad de accurate es falsa significa que se va a comparar el documento como un string completo
-    if(accurate == false){
-        //Damos valor a las propiedades
-        this.name = name.toLowerCase();
-        this.n = n;
-        
-        if(n == undefined && this.name != "Levensthein"){
-            throw new Error("if you use a ngram based algorithm you must indicates the length of the gram");
-        }
-    
-        if(this.name == "Levenshtein"){
-            this.algorithm = new Levensthein();
-        }
-        else if(this.name == "Jaccard"){
-            this.algorithm = new Jaccard(this.n)
-        }
-        else if(this.name == "Kondrak"){
-            this.algorithm = new Ngram(this.n);
-        } 
-        else {
-            throw new Error("The algorithm name specified does not exist");    
-        }
+    //Damos valor a las propiedades
+    this.name = name.toLowerCase();
+    this.n = n;
+
+    if(n == undefined && (this.name == "levenshtein" || this.name == "kondrak")){
+        throw new Error("if you use a ngram based algorithm you must indicates the length of the gram");
+    }
+
+    if(this.name == "levenshtein"){
+        this.algorithm = new Levensthein();
+    }
+    else if(this.name == "jaccard"){
+        this.algorithm = new Jaccard(this.n)
+    }
+    else if(this.name == "kondrak"){
+        this.algorithm = new Ngram(this.n);
     } 
+    else {
+        throw new Error("The algorithm name specified does not exist");    
+    }
+    
 }
 
 
